aa15cace7d2c7234c74f737952cfbc9135568cc5
user <user@gmail.com>
2022-11-19 00:43:09 +0200

    Find instance

--- myApp.js
+++ myApp.js
@@ -53,8 +53,12 @@ const createManyPeople = (arrayOfPeople, done) => {
   });
 };
 
+// Find instance
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  })
 };
 
 const findOneByFood = (food, done) => {
